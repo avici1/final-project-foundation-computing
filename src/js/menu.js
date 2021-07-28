da97319b718af39ce87bd5eb6f10dc8ac4eef65f
Alain Christian <48126545+gal1l0@users.noreply.github.com>
2021-07-28 21:57:18 +0200

    started cart integration

--- src/js/menu.js
+++ src/js/menu.js
@@ -13,7 +13,7 @@ export const FillElements = async () => {
 
 export const addToCart = (id) => {
     console.log('called');
-    window.localStorage.setItem(`item-${id}`, id);
+    window.localStorage.setItem(`${id}`, id);
 }
 function FetchMeals(query, MenuCards, container) {
     fetch(`${apiUrl}/food/menuItems/search?apiKey=${apiKey}&query=${query}`)
@@ -58,4 +58,42 @@ function FetchMeals(query, MenuCards, container) {
             }
         });
     return MenuCards;
+}
+
+export function populateCartData() {
+    let arr = [];
+    const wl = window.localStorage;
+    for (let i = 0; i < wl.length - 1; i++) {
+        const el = wl.key(i);
+        console.log(el.split('-')[0]);
+        if (el.split('-')[0] === 'loglevel') {
+            continue;
+        }
+        fetch(`${apiUrl}/food/menuItems/${el.split('-')[0]}?apiKey=${apiKey}`)
+            .then(response => response.json())
+            .then(item => {
+                arr.push({ id: el.split('-')[0], price: el.split('-')[1], title: item.title });
+                console.log(item);
+            })
+            const container = document.getElementById('table-body');
+            const cartData = arr.map(el => {
+                const tableRow = document.createElement('tr');
+                const tableHeader = document.createElement('th');
+                setElementAttribute(tableHeader, 'scope', 'row');
+                const colId = document.createElement('td');
+                colId.innerText = el.id;
+                const colTitle = document.createElement('td');
+                colTitle.innerText = el.title;
+                const colQty = document.createElement('td');
+                colQty.innerText = el.qty;
+                const colPrice = document.createElement('td');
+                colPrice.innerText = el.price;
+                tableRow.append(colId);
+                tableRow.append(colTitle);
+                tableRow.append(colQty);
+                tableRow.append(colPrice);
+                container.append(cartData);
+            })
+
+    }
 }
